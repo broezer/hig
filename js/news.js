97dabf65da8ecdb48f452ea6d8b17025e1bd833b
broezer@gmail.com <broezer@gmail.com>
2016-11-15 16:58:18 +0100

    add pushberichten

--- js/news.js
+++ js/news.js
@@ -10,13 +10,31 @@ var higApp = angular.module('hig', ['ngSanitize', 'ngTouch', 'angular-carousel']
 
 
 higApp.controller('NewsCtrl', function ($scope, $http) {
+  $scope.blocks = [];
+  $scope.news = [];
+  $scope.pushbericht = [];
 
   $http.get('http://www.hetindustriegebouw.nl/wp-json/wp/v2/nieuwsbrief')
    .success(function(data){
-     $scope.news = data[0];
-     $scope.blocks = data[0].acf.nieuwsbrief_blokken;
-     //console.log($scope.blocks);
+     $scope.news = data[0].acf.nieuwsbrief_blokken;
+     both();
    });
+
+   $http.get('http://www.hetindustriegebouw.nl/wp-json/wp/v2/pushbericht')
+    .success(function(data){
+      $scope.first = data[0];
+      both();
+    });
+
+   var both = function(){
+     if ( $scope.news.length > 0) {
+       $scope.news.push($scope.first);
+       $scope.blocks = $scope.news ;
+       //console.log($scope.blocks);
+     }
+   }
+
+
 });
 
 
